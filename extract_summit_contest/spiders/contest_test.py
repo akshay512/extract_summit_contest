311a2403efefc99e9ee5c2baea7844a96849f3bd
akshay512 <akshay.aa67@gmail.com>
2022-09-28 19:48:18 +0530

    removed item_loader

--- extract_summit_contest/spiders/contest_test.py
+++ extract_summit_contest/spiders/contest_test.py
@@ -1,7 +1,7 @@
 import json
 
 import scrapy
-from ..items import TestItemLoader
+# from ..items import TestItemLoader
 
 
 class ContestTestSpider(scrapy.Spider):
@@ -20,9 +20,12 @@ class ContestTestSpider(scrapy.Spider):
         recommended_links = response.css(".team-item a")
         yield from response.follow_all(recommended_links, self.parse_item)
 
-        il = TestItemLoader(response=response)
-        il.add_css("item_id", "#uuid::text")
-        il.add_css("name", "h2.heading-colored::text")
+        # il = TestItemLoader(response=response)
+        # il.add_css("item_id", "#uuid::text")
+        # il.add_css("name", "h2.heading-colored::text")
+
+        item = {'item_id': response.css('#uuid::text').get(),
+                'name': response.css('h2.heading-colored::text').get()}
 
         image_id_css = ".img-shadow ::attr(src)"
         image_id_pattern = r"/([\da-f-]+)\.jpg"
@@ -31,23 +34,23 @@ class ContestTestSpider(scrapy.Spider):
             script_xpath = "//script[contains(text(), 'mainimage')]"
             image_id = response.xpath(script_xpath).re_first(image_id_pattern)
         if image_id:
-            il.add_value("image_id", image_id)
+            item['image_id']= image_id
 
         rating = response.css('p:contains("Rating") span::text').get()
         if "NO RATING" in rating:
             yield from response.follow_all(
                 response.css("::attr(data-price-url)"),
                 callback=self.parse_rating,
-                cb_kwargs={"item": il.load_item()},
+                cb_kwargs={"item": item},
             )
             return
-        il.add_value("rating", rating)
+        item['rating']=rating
 
-        yield il.load_item()
+        yield item
 
     def parse_rating(self, response, item):
+        item_res = item
         data = json.loads(response.text)
-        il = TestItemLoader(item=item)
-        il.add_value("rating", data.get("value"))
-        yield il.load_item()
+        item_res['rating']= data.get("value")
+        yield item_res
 
